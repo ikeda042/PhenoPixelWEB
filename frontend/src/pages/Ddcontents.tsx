7a8942df09fe1743c1ff200c9962e338d576dec6
ikeda042 <ikea042main@gmail.com>
2024-03-30 03:36:20 +0900

    T

--- frontend/src/pages/Ddcontents.tsx
+++ frontend/src/pages/Ddcontents.tsx
@@ -1,13 +1,13 @@
 
 import * as React from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import Link from '@mui/material/Link';
+import { DataGrid, GridColDef, GridRenderCellParams, } from '@mui/x-data-grid';
 import { Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
 import Grid from '@mui/material/Unstable_Grid2';
+import Link from '@mui/material/Link';
 
 
 type RowData = {
@@ -20,37 +20,6 @@ type RowData = {
 };
 
 
-
-const convertToCSV = (objArray: RowData[]): string => {
-    const array = Array.isArray(objArray) ? objArray : JSON.parse(objArray);
-    let str = `${columns.map(({ headerName }) => `"${headerName}"`).join(",")}\r\n`;
-
-    return array.reduce((str: string, next: RowData) => {
-        str += `${columns.map(({ field }) => `"${next[field]}"`).join(",")}\r\n`;
-        return str;
-    }, str);
-};
-
-const downloadCSV = (csvContent: string, fileName: string): void => {
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.setAttribute('download', fileName);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-};
-
-
-
-
-const columns: GridColDef[] = [
-    { field: 'cell_id', headerName: 'Cell ID', width: 200, align: 'center', headerAlign: 'center' },
-    { field: 'label_experiment', headerName: 'Label Experiment', width: 200, align: 'center', headerAlign: 'center' },
-    { field: 'manual_label', headerName: 'Manual Label', width: 200, align: 'center', headerAlign: 'center' },
-    { field: 'perimeter', headerName: 'Perimeter (µm)', width: 200, align: 'center', headerAlign: 'center' },
-    { field: 'area', headerName: 'Area (µm^2)', width: 200, align: 'center', headerAlign: 'center' }
-];
 
 
 export default function Dbcontents() {
@@ -70,6 +39,40 @@ export default function Dbcontents() {
                 setRows(rowsWithIds);
             });
     }, [filename]);
+
+    const convertToCSV = (objArray: RowData[]): string => {
+        const array = Array.isArray(objArray) ? objArray : JSON.parse(objArray);
+        let str = `${columns.map(({ headerName }) => `"${headerName}"`).join(",")}\r\n`;
+
+        return array.reduce((str: string, next: RowData) => {
+            str += `${columns.map(({ field }) => `"${next[field]}"`).join(",")}\r\n`;
+            return str;
+        }, str);
+    };
+
+    const downloadCSV = (csvContent: string, fileName: string): void => {
+        const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.setAttribute('download', fileName);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+
+
+    const columns: GridColDef[] = [
+        {
+            field: 'cell_id', headerName: 'Cell ID', width: 200, align: 'center', headerAlign: 'center',
+            renderCell: (params: GridRenderCellParams) =>
+                <Link href={`/dbcontents/${filename}/cell/${(params.value as string).split('.')[0]}`}>{(params.value as string).split('.')[0]}</Link>,
+        },
+        { field: 'label_experiment', headerName: 'Label Experiment', width: 200, align: 'center', headerAlign: 'center' },
+        { field: 'manual_label', headerName: 'Manual Label', width: 200, align: 'center', headerAlign: 'center' },
+        { field: 'perimeter', headerName: 'Perimeter (µm)', width: 200, align: 'center', headerAlign: 'center' },
+        { field: 'area', headerName: 'Area (µm^2)', width: 200, align: 'center', headerAlign: 'center' }
+    ];
 
     return (
         <div style={{ height: 700, width: '100%' }}>
