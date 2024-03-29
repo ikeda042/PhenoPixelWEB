ff374ca23dcb27bd6977701400d3cdac03fc03f0
ikeda042 <ikea042main@gmail.com>
2024-03-30 03:36:24 +0900

    T

--- backend/router.py
+++ backend/router.py
@@ -4,8 +4,12 @@ import aiofiles.os as aos
 import asyncio
 import os
 from schemas import DBInfo, CellDB
-from database import count_valid_cells, get_cells
+from database import count_valid_cells, get_cells, get_cell
 from fastapi.middleware.cors import CORSMiddleware
+import cv2
+import numpy as np
+from fastapi.responses import StreamingResponse
+import aiofiles
 
 
 app = FastAPI(docs_url="/docs")
@@ -37,6 +41,16 @@ async def read_cell_dbs():
     for file in await loop.run_in_executor(None, lambda: [entry.name for entry in os.scandir("./databases/") if entry.is_file() and entry.name.endswith(".db")]):
         dbinfo_list.append(DBInfo(file_name=file, cell_count=await count_valid_cells(f"./databases/{file}")))
     return dbinfo_list
+
+@router_cell.get("/cells/{db_name}/cell/{cell_id}/ph")
+async def read_cell_ph(db_name: str, cell_id: str):
+    cell: bytes = await get_cell(f"./databases/{db_name}.db", cell_id)
+    image_ph = cv2.imdecode(np.frombuffer(cell, dtype=np.uint8), cv2.IMREAD_COLOR)
+    _, buffer = cv2.imencode(".png", image_ph)
+    async with aiofiles.open("temp.png", "wb") as afp:
+        await afp.write(buffer)
+    return StreamingResponse(open("temp.png", "rb"), media_type="image/png")
+
 
 
 
