8550843f17662da64233cec559afcdd2c78f187f
ikeda042 <ikea042main@gmail.com>
2024-03-29 23:45:17 +0900

    t

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css';
 import Nav from './components/Nav';
 import { Box } from '@mui/system';
-import SquareImage
-  from './components/Squareimage';
+import SquareImage from './components/Squareimage';
+import DBtable from './components/Dbtable';
+import Grid from '@mui/material/Unstable_Grid2';
+
 function App() {
   return (
     <>
       <Box sx={{ bgcolor: "#f7f6f5", color: 'black', minHeight: '100vh' }}>
         <Nav />
+        <DBtable />
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60vh' }}>
           <SquareImage imgSrc="testimg.png" size={400} />
         </Box>
