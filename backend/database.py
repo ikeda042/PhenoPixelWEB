e98476abb6289794fcab38f74059f7314e95ef32
ikeda042 <ikea042main@gmail.com>
2024-03-29 22:31:13 +0900

    DB

---
+++ backend/database.py
@@ -0,0 +1,51 @@
+import asyncio
+from sqlalchemy import Column, Integer, String, BLOB, FLOAT
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Cell(Base):
+    __tablename__ = 'cells'
+    id = Column(Integer, primary_key=True)
+    cell_id = Column(String)
+    label_experiment = Column(String)
+    manual_label  = Column(Integer)
+    perimeter = Column(FLOAT)
+    area = Column(FLOAT)
+    img_ph = Column(BLOB) 
+    img_fluo1 = Column(BLOB)
+    img_fluo2 = Column(BLOB) | None
+    contour = Column(BLOB)
+    center_x = Column(FLOAT)
+    center_y = Column(FLOAT)
+
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.future import select
+
+engine = create_async_engine(f'sqlite+aiosqlite:///sk326tri120min.db', echo=False)
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def main():
+
+    await create_tables()
+
+    async_session = sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession
+    )
+
+    async with async_session() as session:
+        result = await session.execute(select(Cell))
+        cells = result.scalars().all()
+
+        for cell in cells:
+            print(cell.cell_id)  
+
+import asyncio
+asyncio.run(main())
