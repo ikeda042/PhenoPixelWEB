770210be87c996d5c5cbd823c9cf8e4791b99c41
ikeda042 <ikea042main@gmail.com>
2024-03-29 22:55:28 +0900

    router

--- backend/router.py
+++ backend/router.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI, APIRouter
 import uvicorn
+import aiofiles.os as aos
+import asyncio
+import os
+from schemas import DBname
 
 app = FastAPI(docs_url="/cellAPI")
 router_cell = APIRouter(prefix="/cellAPI")
@@ -9,6 +13,12 @@ router_cell = APIRouter(prefix="/cellAPI")
 async def root():
     return {"message": "Hello World"}
 
+
+@router_cell.get("/cells/databases", response_model=list[DBname])
+async def get_database_names():
+    loop = asyncio.get_event_loop()
+    files = await loop.run_in_executor(None, lambda: [entry.name for entry in os.scandir(".") if entry.is_file() and entry.name.endswith(".db")])
+    return [DBname(name=file) for file in files]
 
 app.include_router(router_cell)
 
---
+++ backend/schemas.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+
+
+class DBname(BaseModel):
+    name: str
