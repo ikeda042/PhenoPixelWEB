5c0038df7dd7fc6bcef3ed6090c935cae9c1dbe3
ikeda042 <ikea042main@gmail.com>
2024-03-30 00:02:28 +0900

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,37 @@
+import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import { Box } from '@mui/system';
 import SquareImage from './components/Squareimage';
 import DBtable from './components/Dbtable';
+import Dbcontents from './pages/Ddcontents';
 import Grid from '@mui/material/Unstable_Grid2';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Import Routes
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Box sx={{ bgcolor: "#f7f6f5", color: 'black', minHeight: '100vh' }}>
-
         <Nav />
-        <Grid container spacing={4} margin={5}>
-          <DBtable />
-        </Grid >
+        <Routes>
 
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60vh' }}>
-          <SquareImage imgSrc="testimg.png" size={400} />
-        </Box>
+          <Route path="/" element={
+            <>
+              <Link to="/dbcontents">DbContents</Link>
+              <Grid container spacing={4} margin={5}>
+                <DBtable />
+              </Grid >
+              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60vh' }}>
+                <SquareImage imgSrc="testimg.png" size={400} />
+              </Box>
+            </>
+          } />
+
+          <Route path="/dbcontents" element={<Dbcontents />} />
+        </Routes>
       </Box >
-    </>
+    </Router>
   );
 }
 
--- frontend/src/pages/Ddcontents.tsx
+++ frontend/src/pages/Ddcontents.tsx
@@ -1,26 +1,18 @@
-import Box from '@mui/material/Box';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
-const columns: GridColDef<(typeof rows)[number]>[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-        field: 'firstName',
-        headerName: 'First name',
-        width: 150,
-        editable: true,
-    },
-    {
-        field: 'lastName',
-        headerName: 'Last name',
-        width: 150,
-        editable: true,
-    },
+import * as React from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
+
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
     {
         field: 'age',
         headerName: 'Age',
         type: 'number',
-        width: 110,
-        editable: true,
+        width: 90,
     },
     {
         field: 'fullName',
@@ -33,10 +25,10 @@ const columns: GridColDef<(typeof rows)[number]>[] = [
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
@@ -44,16 +36,21 @@ const rows = [
     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
-export default function DBtable() {
+export default function Dbcontents() {
     return (
-        <Box sx={{ height: 500, width: '100%' }}>
+        <div style={{ height: 400, width: '100%' }}>
+            <Link to="/">DbContents</Link>
             <DataGrid
                 rows={rows}
                 columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                }}
+                pageSizeOptions={[5, 10]}
                 checkboxSelection
-                disableRowSelectionOnClick
-                hideFooterPagination
             />
-        </Box>
+        </div>
     );
 }
