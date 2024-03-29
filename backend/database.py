1d5901d2f766b166f8378c9788de4aed9f74ad6c
ikeda042 <ikea042main@gmail.com>
2024-03-30 03:57:11 +0900

    db

--- backend/database.py
+++ backend/database.py
@@ -59,3 +59,9 @@ async def get_cell_fluo(db_name:str, cell_id:str) -> bytes:
         result = await session.execute(select(Cell).where(Cell.cell_id == cell_id))
         cell = result.scalars().first()
         return cell.img_fluo1
+
+async def get_cell_contour(db_name:str, cell_id:str) -> bytes:
+    async for session in get_session(db_name):
+        result = await session.execute(select(Cell).where(Cell.cell_id == cell_id))
+        cell = result.scalars().first()
+        return cell.contour
