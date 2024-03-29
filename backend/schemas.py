392f21cce7ad045a8ad3c09f2916d142d20eae41
ikeda042 <ikea042main@gmail.com>
2024-03-30 01:53:34 +0900

    CellDB schema

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
 
 
+class CellDB(BaseModel):
+    cell_id: str
+    label_experiment: str
+    manual_label: int
+    perimeter: float
+    area: float
 
 class DBInfo(BaseModel):
     file_name: str
