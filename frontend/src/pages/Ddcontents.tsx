4b0ee116fe2084a969b2d119e603402fc51b6fa2
ikeda042 <ikea042main@gmail.com>
2024-03-30 03:04:26 +0900

    T

--- frontend/src/pages/Ddcontents.tsx
+++ frontend/src/pages/Ddcontents.tsx
@@ -5,6 +5,28 @@ import Link from '@mui/material/Link';
 import { Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
+
+
+const convertToCSV = (objArray: RowData[]): string => {
+    const array = Array.isArray(objArray) ? objArray : JSON.parse(objArray);
+    let str = `${Object.keys(array[0]).map(value => `"${value}"`).join(",")}\r\n`;
+
+    return array.reduce((str: string, next: RowData) => {
+        str += `${Object.values(next).map(value => `"${value}"`).join(",")}\r\n`;
+        return str;
+    }, str);
+};
+
+const downloadCSV = (csvContent: string, fileName: string): void => {
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.setAttribute('download', fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
 
 type RowData = {
     cell_id: string;
@@ -20,8 +42,8 @@ const columns: GridColDef[] = [
     { field: 'cell_id', headerName: 'Cell ID', width: 200, align: 'center', headerAlign: 'center' },
     { field: 'label_experiment', headerName: 'Label Experiment', width: 200, align: 'center', headerAlign: 'center' },
     { field: 'manual_label', headerName: 'Manual Label', width: 200, align: 'center', headerAlign: 'center' },
-    { field: 'perimeter', headerName: 'Perimeter(µm)', width: 200, align: 'center', headerAlign: 'center' },
-    { field: 'area', headerName: 'Area (µm^2)', width: 200, align: 'center', headerAlign: 'center' }
+    { field: 'perimeter (µm)', headerName: 'Perimeter(µm)', width: 200, align: 'center', headerAlign: 'center' },
+    { field: 'area (µm^2)', headerName: 'Area (µm^2)', width: 200, align: 'center', headerAlign: 'center' }
 ];
 
 
@@ -29,6 +51,10 @@ export default function Dbcontents() {
     const { filename } = useParams();
     const [rows, setRows] = useState<RowData[]>([]);
 
+    const handleExport = () => {
+        const csvData = convertToCSV(rows);
+        downloadCSV(csvData, `${filename}.csv`);
+    };
 
     useEffect(() => {
         fetch(`http://10.32.17.15:8000/cellapi/cells/databases/${filename}`)
@@ -46,6 +72,9 @@ export default function Dbcontents() {
             <Typography variant="h4" component="h2" gutterBottom>
                 {filename}
             </Typography>
+            <Button variant="contained" onClick={handleExport} style={{ marginBottom: '10px' }}>
+                Export to CSV
+            </Button>
             <DataGrid
                 rows={rows}
                 columns={columns}
