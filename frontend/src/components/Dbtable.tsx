512732d1b45e66720787b750a35759d0f1cd3b59
ikeda042 <ikea042main@gmail.com>
2024-03-30 01:00:00 +0900

    T

--- frontend/src/components/Dbtable.tsx
+++ frontend/src/components/Dbtable.tsx
@@ -1,13 +1,18 @@
 import { Box } from '@mui/system';
 import { DataGrid } from '@mui/x-data-grid';
+import { useNavigate } from 'react-router-dom';
 
 interface DBTableProps {
     data: { file_name: string; cell_count: number }[];
 }
 
 export default function DBtable({ data }: DBTableProps) {
+    const navigate = useNavigate();
+
     const columns = [
-        { field: 'file_name', headerName: 'File Name', width: 200 },
+        {
+            field: 'file_name', headerName: 'File Name', width: 200
+        },
         { field: 'cell_count', headerName: 'Cell Count', width: 200 },
         { field: 'antibiotics', headerName: 'Antibiotics', width: 200 },
         { field: 'exposure_time', headerName: 'Exposure time (min)', width: 200 },
@@ -32,6 +37,11 @@ export default function DBtable({ data }: DBTableProps) {
             checkboxSelection
             disableRowSelectionOnClick
             hideFooterPagination
+            onCellClick={(params, event) => {
+                if (params.field === 'file_name') {
+                    navigate(`/dbcontents/:${params.value}`);
+                }
+            }}
         />
     );
 }
