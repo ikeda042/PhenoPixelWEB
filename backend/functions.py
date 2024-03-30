cf67c28cec24a574ad316a7ad117e2cadda9729b
ikeda042 <ikea042main@gmail.com>
2024-03-31 03:28:22 +0900

    save await

--- backend/functions.py
+++ backend/functions.py
@@ -1,6 +1,18 @@
 import cv2
 import numpy as np
 from numpy.linalg import eig
+import matplotlib.pyplot as plt
+import pickle
+from numpy.linalg import inv
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
+
+executor = ThreadPoolExecutor(max_workers=1)
+
+async def save_fig_async(fig, filename):
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(executor, fig.savefig, filename)
+    plt.close(fig)
 
 async def draw_scale_bar_with_centered_text(image_ph):
     """
@@ -81,3 +93,135 @@ def basis_conversion(contour:list[list[int]],X:np.ndarray,center_x:float,center_
     u2_contour = [i[0] for i in contour_U]
     min_u1, max_u1 = min(u1), max(u1)
     return u1,u2,u1_contour,u2_contour,min_u1,max_u1,u1_c,u2_c, U, contour_U
+
+
+async def replot_blocking_operations(image_fluo, contour_raw, gray):
+    class Point:
+        def __init__(self, u1: float, G: float):
+            self.u1 = u1
+            self.G = G
+
+        def __gt__(self, other):
+            return self.u1 > other.u1
+    contour = [
+                        [j, i] for i, j in [i[0] for i in pickle.loads(contour_raw)]
+                    ]
+    coords_inside_cell_1, points_inside_cell_1, projected_points = [], [],[]
+    for i in range(image_fluo.shape[1]):
+        for j in range(image_fluo.shape[0]):
+            if (
+                cv2.pointPolygonTest(
+                    pickle.loads(contour_raw), (i, j), False
+                )
+                >= 0
+            ):
+                coords_inside_cell_1.append([i, j])
+                points_inside_cell_1.append(gray[j][i])
+    X = np.array(
+                        [
+                            [i[1] for i in coords_inside_cell_1],
+                            [i[0] for i in coords_inside_cell_1],
+                        ]
+                    )
+    (
+                        u1,
+                        u2,
+                        u1_contour,
+                        u2_contour,
+                        min_u1,
+                        max_u1,
+                        u1_c,
+                        u2_c,
+                        U,
+                        contour_U,
+                    ) = basis_conversion(
+                        contour, X, image_fluo.shape[0]//2, image_fluo.shape[1]//2, coords_inside_cell_1
+                    )   
+    min_u1, max_u1 = min(u1), max(u1)
+    fig = plt.figure(figsize=[6, 6])
+    cmap = plt.get_cmap("inferno")
+    x = np.linspace(0, 100, 1000)
+    max_points = max(points_inside_cell_1)
+    plt.scatter(
+        u1,
+        u2,
+        c=[i / max_points for i in points_inside_cell_1],
+        s=10,
+        cmap=cmap,
+    )
+    # plt.scatter(u1_contour, u2_contour, s=10, color="lime")
+    W = np.array([[i**4, i**3, i**2, i, 1] for i in [i[1] for i in U]])
+    f = np.array([i[0] for i in U])
+    theta = inv(W.transpose() @ W) @ W.transpose() @ f
+    x = np.linspace(min_u1, max_u1, 1000)
+    y = [
+        theta[0] * i**4
+        + theta[1] * i**3
+        + theta[2] * i**2
+        + theta[3] * i
+        + theta[4]
+        for i in x
+    ]
+    plt.plot(x, y, color="blue", linewidth=1)
+    plt.scatter(
+        min_u1,
+        theta[0] * min_u1**4
+        + theta[1] * min_u1**3
+        + theta[2] * min_u1**2
+        + theta[3] * min_u1
+        + theta[4],
+        s=100,
+        color="red",
+        zorder=100,
+        marker="x",
+    )
+    plt.scatter(
+        max_u1,
+        theta[0] * max_u1**4
+        + theta[1] * max_u1**3
+        + theta[2] * max_u1**2
+        + theta[3] * max_u1
+        + theta[4],
+        s=100,
+        color="red",
+        zorder=100,
+        marker="x",
+    )
+
+    plt.xlabel("u1")
+    plt.ylabel("u2")
+    plt.axis("equal")
+    plt.xlim(min_u1 - 80, max_u1 + 80)
+    plt.ylim(u2_c - 80, u2_c + 80)
+
+    # Y軸の範囲を取得
+    ymin, ymax = plt.ylim()
+    y_pos = ymin + 0.2 * (ymax - ymin)
+    y_pos_text = ymax - 0.15 * (ymax - ymin)
+    plt.text(
+        u1_c,
+        y_pos_text,
+        s=f"",
+        color="red",
+        ha="center",
+        va="top",
+    )
+    for u, g in zip(u1, points_inside_cell_1):
+        point = Point(u, g)
+        projected_points.append(point)
+    sorted_projected_points = sorted(projected_points)
+    # add second axis
+    ax2 = plt.twinx()
+    ax2.grid(False)
+    ax2.set_xlabel("u1")
+    ax2.set_ylabel("Brightness")
+    ax2.set_ylim(0, 900)
+    ax2.set_xlim(min_u1 - 40, max_u1 + 40)
+    ax2.scatter(
+        [i.u1 for i in sorted_projected_points],
+        [i.G for i in sorted_projected_points],
+        color="lime",
+        s=1,
+    )
+    await save_fig_async(fig, "temp_replot.png")
+    plt.close()
