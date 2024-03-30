a7a261492edaac12b5dad3e2b0f0f004e4ee99c0
ikeda042 <ikea042main@gmail.com>
2024-03-31 03:41:17 +0900

    export db

--- frontend/src/components/Dbtable.tsx
+++ frontend/src/components/Dbtable.tsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/system';
 import { DataGrid } from '@mui/x-data-grid';
 import { GridRenderCellParams, GridColDef } from '@mui/x-data-grid';
 import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
 
 interface DBTableProps {
     data: { file_name: string; cell_count: number }[];
@@ -22,7 +23,23 @@ export default function DBtable({ data }: DBTableProps) {
         { field: 'cell_count', headerName: 'Cell Count', width: 200, align: 'center', headerAlign: 'center' },
         { field: 'antibiotics', headerName: 'Antibiotics', width: 200, align: 'center', headerAlign: 'center' },
         { field: 'exposure_time', headerName: 'Exposure time (min)', width: 200, align: 'center', headerAlign: 'center' },
-        { field: 'strain_code', headerName: 'Strain code', width: 200, align: 'center', headerAlign: 'center' }
+        { field: 'strain_code', headerName: 'Strain code', width: 200, align: 'center', headerAlign: 'center' },
+        {
+            field: 'action',
+            headerName: 'Export database',
+            width: 150,
+            align: 'center',
+            headerAlign: 'center',
+            renderCell: (params: GridRenderCellParams) => (
+                <Button
+                    variant="contained"
+                    style={{ backgroundColor: 'black', color: 'white' }}
+                    onClick={() => console.log(`Clicked row ${params.id} with file name ${(params.row.file_name as string).split('.')[0]}`)}
+                >
+                    Export
+                </Button>
+            ),
+        },
     ];
 
     return (
@@ -33,9 +50,13 @@ export default function DBtable({ data }: DBTableProps) {
                 return {
                     id: index,
                     ...row,
-                    antibiotics: row.file_name.includes('tri') ? 'Triclosan' : row.file_name,
+                    antibiotics: row.file_name.includes('tri') ? 'Triclosan'
+                        : row.file_name.includes('cip') ? 'Ciprofloxacin'
+                            : row.file_name.includes('gen') ? 'Gentamicin'
+                                : row.file_name.includes('amp') ? 'Ampicillin'
+                                    : row.file_name,
                     exposure_time: exposure_time,
-                    strain_code: Number(row.file_name.match(/\d+/)?.[0] ?? 0)
+                    strain_code: row.file_name.includes('pi') ? 'PI' : Number(row.file_name.match(/\d+/)?.[0] ?? 0)
                 };
             })}
             columns={columns}
