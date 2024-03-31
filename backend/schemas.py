6059ce8db86cf066e3357888d3a7e1599e747ce6
ikeda042 <ikea042main@gmail.com>
2024-03-31 12:11:43 +0900

    celldball

--- backend/schemas.py
+++ backend/schemas.py
@@ -11,3 +11,16 @@ class CellDB(BaseModel):
 class DBInfo(BaseModel):
     file_name: str
     cell_count: int
+
+class CellDBAll(BaseModel):
+    cell_id: str
+    label_experiment: str
+    manual_label: int
+    perimeter: float
+    area: float
+    img_ph: bytes
+    img_fluo1: bytes
+    img_fluo2: bytes | None
+    contour: bytes
+    center_x: float
+    center_y: float
